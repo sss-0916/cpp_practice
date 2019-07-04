86ab08f5086e5b4e722f502f52be554717106663
sss-0916 <sss-0916@qq.com>
2019-07-04 21:17:32 +0800

    最近公共祖先

---
+++ 21_lowest_common_ancestor.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class LCA{
+	public:
+		int getLCA(int a, int b){
+			while(a != b){
+				if(a > b){
+					a /= 2;
+				}
+				else{
+					b /= 2;
+				}
+			}
+
+			return a;
+		}
+};
